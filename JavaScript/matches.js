07d4b7943d25aa48235aaa10f8f3dfb9d511a9ab
Miikii97 <milenovikjmiki@gmail.com>
2025-11-13 20:21:44 +0100

    Added calendar

--- JavaScript/matches.js
+++ JavaScript/matches.js
@@ -200,3 +200,12 @@ let matches = [
             sport: "football" 
         } 
 ];  
+
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
